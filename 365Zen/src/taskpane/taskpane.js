493b0662742189d47388441ff12f1ccc68b6e708
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-06-08 19:23:26 -0400

    sadas
    sdf

--- 365Zen/src/taskpane/taskpane.js
+++ 365Zen/src/taskpane/taskpane.js
@@ -3,16 +3,6 @@
  * See LICENSE in the project root for license information.
  */
 
-
-
-Office.onReady(info => {
-  if (info.host === Office.HostType.Outlook) {
-    document.getElementById("insertTemplate1").onclick = sendTemplate1;
-    document.getElementById("insertSubjectButton").onclick = insertSubject;
-    document.getElementById("insertBodyButton").onclick = insertBody;
-  }
-});
-
 function sendTemplate1() {
   Office.context.mailbox.item.subject.setAsync("Hello World!");
   Office.context.mailbox.item.body.setAsync("This is the body of the email.");
@@ -25,6 +15,16 @@ function insertSubject() {
 function insertBody() {
   Office.context.mailbox.item.body.setAsync("This is the body of the email.");
 }
+
+
+Office.onReady(info => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("insertTemplate1").onclick = sendTemplate1;
+    document.getElementById("insertSubjectButton").onclick = insertSubject;
+    document.getElementById("insertBodyButton").onclick = insertBody;
+  }
+});
+
 
 
 
