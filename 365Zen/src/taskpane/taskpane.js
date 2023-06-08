9edd37335b5b55abc3caa128bb5910a10cda05cb
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-06-08 19:50:04 -0400

    dfgdfg
    sdgf

--- 365Zen/src/taskpane/taskpane.js
+++ 365Zen/src/taskpane/taskpane.js
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-function sendTemplate1() {
-  Office.context.mailbox.item.subject.setAsync("Hello World!");
-  Office.context.mailbox.item.body.setAsync("This is the body of the email.");
-}
-
-function insertSubject() {
-  Office.context.mailbox.item.subject.setAsync("Hello World!");
-}
-
-function insertBody() {
-  Office.context.mailbox.item.body.setAsync("This is the body of the email.");
-}
-
-
-Office.onReady(info => {
-  if (info.host === Office.HostType.Outlook) {
-    document.getElementById("insertTemplate1").onclick = sendTemplate1;
-    document.getElementById("insertSubjectButton").onclick = insertSubject;
-    document.getElementById("insertBodyButton").onclick = insertBody;
-  }
-});
-
-
-
-
-
 
 
 
