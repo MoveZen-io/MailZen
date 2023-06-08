12e1b4a0f4616a74bc23084fa6d7548e863676d7
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-06-08 19:26:48 -0400

    sdf
    asdf

--- 365Zen/src/commands/commands.js
+++ 365Zen/src/commands/commands.js
@@ -1,12 +1,9 @@
-Office.onReady(info => {
-  if (info.host === Office.HostType.Outlook) {
-    document.getElementById("setTextButton").onclick = setText;
-    document.getElementById("insertSubjectButton").onclick = insertSubject;
-    document.getElementById("insertBodyButton").onclick = insertBody;
-  }
-});
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
 
-function setText() {
+function sendTemplate1() {
   Office.context.mailbox.item.subject.setAsync("Hello World!");
   Office.context.mailbox.item.body.setAsync("This is the body of the email.");
 }
@@ -18,6 +15,17 @@ function insertSubject() {
 function insertBody() {
   Office.context.mailbox.item.body.setAsync("This is the body of the email.");
 }
+
+
+Office.onReady(info => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("insertTemplate1").onclick = sendTemplate1;
+    document.getElementById("insertSubjectButton").onclick = insertSubject;
+    document.getElementById("insertBodyButton").onclick = insertBody;
+  }
+});
+
+
 
 
 
