9d314c934bfc606c3c20b9713a41331fdcbbdb81
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-06-08 08:29:06 -0400

    sdfs
    sdf

--- 365Zen/onInit.js
+++
@@ -1,8 +0,0 @@
-function onInit(context) {
-    // Pin the add-in's button to the ribbon.
-    var ribbon = context.ui.ribbon;
-    var tab = ribbon.tabs.getById("TabDefault");
-    var group = tab.groups.getById("msgSendGroup");
-    var control = group.controls.getById("msgSendOpenPaneButton");
-    control.pinnable = true;
-  }
