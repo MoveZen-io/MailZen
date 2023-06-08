f294b360cafbbaea9660de20bfaad931121ff1d7
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-06-08 19:18:14 -0400

    sdf
    sdf

--- 365Zen copy/src/taskpane/taskpane.js
+++ 365Zen copy/src/taskpane/taskpane.js
@@ -7,13 +7,13 @@
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
-    document.getElementById("insertTemplate1").onclick = sendTemplate1;
+    document.getElementById("setTextButton").onclick = setText;
     document.getElementById("insertSubjectButton").onclick = insertSubject;
     document.getElementById("insertBodyButton").onclick = insertBody;
   }
 });
 
-function sendTemplate1() {
+function setText() {
   Office.context.mailbox.item.subject.setAsync("Hello World!");
   Office.context.mailbox.item.body.setAsync("This is the body of the email.");
 }
