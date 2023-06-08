d36dced0d7c7de2be819c712301d4b7c58dccd7c
RE Hunter <45364073+rehunter145@users.noreply.github.com>
2023-06-08 19:07:55 -0400

    buttons ready to test
    sdaf

--- 365Zen/src/taskpane/taskpane.js
+++ 365Zen/src/taskpane/taskpane.js
@@ -3,18 +3,59 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global document, Office */
 
-Office.onReady((info) => {
+
+Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("setTextButton").onclick = setText;
+    document.getElementById("insertSubjectButton").onclick = insertSubject;
+    document.getElementById("insertBodyButton").onclick = insertBody;
   }
 });
 
-export async function run() {
-  /**
-   * Insert your Outlook code here
-   */
+function setText() {
+  Office.context.mailbox.item.subject.setAsync("Hello World!");
+  Office.context.mailbox.item.body.setAsync("This is the body of the email.");
 }
+
+function insertSubject() {
+  Office.context.mailbox.item.subject.setAsync("Hello World!");
+}
+
+function insertBody() {
+  Office.context.mailbox.item.body.setAsync("This is the body of the email.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* global document, Office */
+
+// Office.onReady((info) => {
+//   if (info.host === Office.HostType.Outlook) {
+//     document.getElementById("sideload-msg").style.display = "none";
+//     document.getElementById("app-body").style.display = "flex";
+//     document.getElementById("run").onclick = run;
+//   }
+// });
+
+// export async function run() {
+//   /**
+//    * Insert your Outlook code here
+//    */
+// }
+
+
